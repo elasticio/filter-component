e639965846e5e6fc972356f2232b30e8480ac3f3
Renat Zubairov <renat.zubairov@gmail.com>
2017-01-12 17:42:35 +0100

    Removed verifyCredentials.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 /*eslint func-names: 0*/
 module.exports = function configure(grunt) {
 
-  grunt.initConfig({
-    jasmine_node: {
-      options: {
-        forceExit: true,
-        match: '.',
-        matchall: false,
-        extensions: 'js',
-        specNameMatcher: 'spec'
-      },
-      all: ['spec/']
-    }
-  });
+    grunt.initConfig({
+        jasmine_node: {
+            options: {
+                forceExit: true,
+                match: '.',
+                matchall: false,
+                extensions: 'js',
+                specNameMatcher: 'spec'
+            },
+            all: ['spec/']
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-jasmine-node');
+    grunt.loadNpmTasks('grunt-jasmine-node');
 
-  grunt.registerTask('default', 'jasmine_node');
+    grunt.registerTask('default', 'jasmine_node');
 
 };
