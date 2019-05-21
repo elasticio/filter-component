932f4d698c104e04d3f50aaeef3f12332c1d5e05
Zain Noor <zainnoor516@gmail.com>
2019-05-21 11:19:20 +0200

    Test for new filter

---
+++ spec/simpleJSONataFilter.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+const assert = require('chai').assert;
+const action = require('../lib/actions/simpleJSONataFilter').process;
+
+describe('Test filter', () => {
+
+    function filter(condition, passOrFail) {
+        it(condition, (done) => {
+           let dataWasCalled = false;
+
+           function onEmit(type, value) {
+               if (type) {
+                   assert.isDefined(value);
+                   dataWasCalled = true;
+               } else {
+                   assert.isUndefined(value);
+                   assert.equal(dataWasCalled, passOrFail);
+                   done();
+               }
+           }
+
+           const cfg = {
+               condition: condition
+           };
+
+            const msg = {};
+            action.call({
+               emit: onEmit
+            }, msg, cfg);
+        });
+    }
+
+    describe(' should pass ', () => {
+        filter('true', true);
+        filter('!false', true);
+        filter('body.foo > 5', true);
+        filter('parseFloat(body.flString) > 2', true);
+        filter('body.flString > 20', true);
+        filter('moment(body.iso8601).day() == 1', true);
+    });
+
+    describe(' should fail ', () => {
+        filter('false', false);
+        filter('!true', false);
+        filter('body.foo > 20', false);
+        filter('body.float > 20.4', false);
+    });
+
+});
