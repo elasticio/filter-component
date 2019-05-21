92d255c7a13d367a2701475a6a713392b208297c
Zain Noor <zainnoor516@gmail.com>
2019-05-21 12:21:33 +0200

    Added test

--- spec/simpleJSONataFilter.spec.js
+++ spec/simpleJSONataFilter.spec.js
@@ -3,34 +3,51 @@ const assert = require('chai').assert;
 const action = require('../lib/actions/simpleJSONataFilter').process;
 
 describe('Test filter', () => {
-
     function filter(condition, passOrFail) {
+        let msg = {};
+        let cfg = {
+            condition: condition
+        }
+        let actionObject = action(msg, cfg);
         it(condition, (done) => {
-           let dataWasCalled = false;
+            let eventEmitted = false;
 
-           function onEmit(type, value) {
-               if (type) {
-                   assert.isDefined(value);
-                   dataWasCalled = true;
-               } else {
-                   assert.isUndefined(value);
-                   assert.equal(dataWasCalled, passOrFail);
-                   done();
-               }
-           }
-
-           const cfg = {
-               condition: condition
-           };
-
-            const msg = {};
-            action.call({
-               emit: onEmit
-            }, msg, cfg);
+            if (actionObject.on('data', () => {
+                eventEmitted = true;
+            })) {
+                assert.equal(eventEmitted, passOrFail);
+            } else {
+                assert.equal(eventEmitted, passOrFail);
+            }
         });
     }
+    // function filter(condition, passOrFail) {
+    //     it(condition, (done) => {
+    //        let dataWasCalled = false;
+    //
+    //        function onEmit(type, value) {
+    //            if (type) {
+    //                assert.isDefined(value);
+    //                dataWasCalled = true;
+    //            } else {
+    //                assert.isUndefined(value);
+    //                assert.equal(dataWasCalled, passOrFail);
+    //                done();
+    //            }
+    //        }
+    //
+    //        const cfg = {
+    //            condition: condition
+    //        };
+    //
+    //         const msg = {};
+    //         action.call({
+    //            emit: onEmit
+    //         }, msg, cfg);
+    //     });
+    // }
 
-    describe(' should pass ', () => {
+    describe(' should fire event ', () => {
         filter('true', true);
         filter('!false', true);
         filter('body.foo > 5', true);
@@ -39,7 +56,7 @@ describe('Test filter', () => {
         filter('moment(body.iso8601).day() == 1', true);
     });
 
-    describe(' should fail ', () => {
+    describe(' should not do anything ', () => {
         filter('false', false);
         filter('!true', false);
         filter('body.foo > 20', false);
