e3718e6d5c1ca04245a06d64e84728777e54a87a
Zain Noor <zainnoor516@gmail.com>
2019-05-22 11:59:59 +0200

    Recommiting file

--- spec/simpleJSONataFilter.spec.js
+++ spec/simpleJSONataFilter.spec.js
@@ -2,97 +2,75 @@
 'use strict';
 
 const assert = require('chai').assert;
-const action = require('../lib/actions/simpleJSONataFilter').process;
-const emitter = require('../lib/actions/simpleJSONataFilter').emitter;
+const jsonata = require('@elastic.io/jsonata-moment');
+const action = require('../lib/actions/simpleJSONataFilter');
 
 describe('Test filter', () => {
-    const msg = {};
+    const msg = {
+        body: {}
+    };
     function filter(condition, passOrFail) {
-        it('Running tests', (done) => {
+        it(condition.expression, (done) => {
             let eventEmitted = false;
             function onEmit(type, value) {
-                if (type) {
-                    assert.isDefined(value);
+                if (type && type === 'data') {
                     eventEmitted = true;
-                    done();
-                } else {
-                    assert.isUndefined(value);
+                    assert.equal(eventEmitted, passOrFail);
+                } else if (type && type === 'end') {
                     assert.equal(eventEmitted, passOrFail);
                     done();
                 }
             }
 
             const cfg = condition;
-            action.call({
+            action.process.call({
                 emit: onEmit
             }, msg, cfg);
         });
+
     }
 
-        //let actionObject = action(msg, cfg);
-    //     it('Running tests', (done) => {
-    //         action(msg, cfg);
-    //         let eventEmitted = false;
-    //         emitter.on('data', () => {
-    //             eventEmitted = true;
-    //         });
-    //         assert.equal(eventEmitted, passOrFail);
-    //         done();
-    //     });
-    // }
 
-    // function filter(condition, passOrFail) {
-    //     it(condition, (done) => {
-    //        let dataWasCalled = false;
-    //
-    //        function onEmit(type, value) {
-    //            if (type) {
-    //                assert.isDefined(value);
-    //                dataWasCalled = true;
-    //            } else {
-    //                assert.isUndefined(value);
-    //                assert.equal(dataWasCalled, passOrFail);
-    //                done();
-    //            }
-    //        }
-    //
-    //        const cfg = {
-    //            condition: condition
-    //        };
-    //
-    //         const msg = {};
-    //         action.call({
-    //            emit: onEmit
-    //         }, msg, cfg);
-    //     });
-    // }
     const passCondition1 = {
-        expression: true
+        expression: 'true'
     };
     const passCondition2 = {
-        expression: !false
+        expression: '!false'
     };
     const passCondition3 = {
-        expression: 20 > 5
+        expression: '20 > 5'
     };
     const passCondition4 = {
-        expression: parseFloat('20.4') > 2
+        expression: '20.4 > 2'
     };
     const passCondition5 = {
-        expression: 20.4 > 20
+        expression: '20.4 > 20'
     };
+
+
     const failCondition1 = {
-        expression: false
+        expression: 'false'
     };
     const failCondition2 = {
-        expression: !true
+        expression: '!true'
     };
     const failCondition3 = {
-        expression: 20 > 20
+        expression: '20 > 20'
     };
     const failCondition4 = {
-        expression: 20.4 > 20.4
+        expression: '20.4 > 20.4'
     };
+    const failCondition5 = {
+        expression: 'null'
+    };
+    const failCondition6 = {
+        expression: 'undefined'
+    };
+    const failCondition7 = {
+        expression: ''
+    };
+
+
     describe(' should fire event ', () => {
         filter(passCondition1, true);
         filter(passCondition2, true);
@@ -106,6 +84,9 @@ describe('Test filter', () => {
         filter(failCondition2, false);
         filter(failCondition3, false);
         filter(failCondition4, false);
+        filter(failCondition5, false);
+        filter(failCondition6, false);
+        filter(failCondition7, false);
     });
 
 });
