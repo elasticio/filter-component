55a7a82f15b56e7b62c805376e6c7c42c798403b
Zain Noor <zainnoor516@gmail.com>
2019-05-27 10:34:11 +0200

    Corrected structure of it statements

--- spec/simpleJSONataFilter.spec.js
+++ spec/simpleJSONataFilter.spec.js
@@ -76,25 +76,25 @@ describe('Test filter', () => {
     };
 
 
-    it(' should fire event ', async () => {
-        await filter(passCondition1, true);
-        await filter(passCondition2, true);
-        await filter(passCondition3, true);
-        await filter(passCondition4, true);
-        await filter(passCondition5, true);
+    describe(' should fire event ', async () => {
+        it(passCondition1.expression, async () => {await filter(passCondition1, true);});
+        it(passCondition2.expression, async () => {await filter(passCondition2, true);});
+        it(passCondition3.expression, async () => {await filter(passCondition3, true);});
+        it(passCondition4.expression, async () => {await filter(passCondition4, true);});
+        it(passCondition5.expression, async () => {await filter(passCondition5, true);});
     });
 
-    it(' should not do anything ', async () => {
-        await filter(failCondition1, false);
-        await filter(failCondition2, false);
-        await filter(failCondition3, false);
-        await filter(failCondition4, false);
-        await filter(failCondition5, false);
-        await filter(failCondition6, false);
+    describe(' should just log message to console ', async () => {
+        it(passCondition1.expression, async () => {await filter(failCondition1, false);});
+        it(failCondition2.expression, async () => {await filter(failCondition2, false);});
+        it(failCondition3.expression, async () => {await filter(failCondition3, false);});
+        it(failCondition4.expression, async () => {await filter(failCondition4, false);});
+        it(failCondition5.expression, async () => {await filter(failCondition5, false);});
+        it(failCondition6.expression, async () => {await filter(failCondition6, false);});
     });
 
-    it(' should throw error ', async () => {
-        await errorCondition(errorCondition1);
+    describe(' should throw error ', async () => {
+        it(errorCondition1.expression, async () => {await errorCondition(errorCondition1);});
     });
 
 });
